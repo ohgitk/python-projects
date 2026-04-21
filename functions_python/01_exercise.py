660b7737c29bd20d9151dfd0e946975651d40b1d
guilegarcia <guilesgarcia@gmail.com>
2026-04-21 12:03:42 -0300

    add functions

--- functions_python/01_exercise.py
+++ functions_python/01_exercise.py
@@ -1,3 +1,14 @@
+"""
+## 1. Basic Calculator
 
+Create four separate functions for basic mathematical operations:
 
+- `add(num1, num2)` → returns the sum  
+- `subtract(num1, num2)` → returns the subtraction  
+- `multiply(num1, num2)` → returns the multiplication  
+- `divide(num1, num2)` → returns the division  
+"""
 
+def add(int | float num1, int | float num2): -> int | float:
+    """Returns the sum of num1 and num2."""
+    return num1 + num2
